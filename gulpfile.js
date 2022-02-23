43514cb88ae18ef72df9e9c8af1b98c89e8ab58c
Jose Cobas <julgranen7@gmail.com>
2022-02-23 11:39:49 +0100

    mycket

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -17,10 +17,17 @@ function css (done) {
 }
 
 function dev () {
-  watch('src/scss/app.scss', css);
+  watch('src/scss/**/*.scss', css);
+ // watch('src/scss/app.scss', css);
 }
+
 
 exports.css = css;
 exports.dev = dev;
+exports.default = series( css, dev);
 
 /* Estas lineas de codigo son mandatorias en todos los proyectos permiten el uso de gulp con sass */
+
+// series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente.
+
+// parallel - Todas inician al mismo tiempo.
