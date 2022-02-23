90dd636dd2837c76d4b8ed1e31bc803fbb508c1a
Jose Cobas <julgranen7@gmail.com>
2022-02-23 09:45:50 +0100

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 
 
@@ -8,9 +10,10 @@ function css (done) {
   // pasos: 1- identificar archivo, 2- compilarla, 3-  guardar el css
 
   src('src/scss/app.scss')
-    .pipe(sass({ outputStyle: 'compressed'})) // esto lo que hace es comprimir la carpeta de css, (minificar)
+    .pipe(sass()) // esto lo que hace es comprimir la carpeta de css, (minificar)
+    .pipe(postcss([ autoprefixer()]))
     .pipe(dest('build/css'))
-  done()
+  done();
 }
 
 function dev () {
