362533c6e49e151988b07c24ad99279ebfd12f38
Jose Cobas <julgranen7@gmail.com>
2022-02-23 09:25:24 +0100

    .

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@ const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 
+
 function css (done) {
   // compilar sass
   // pasos: 1- identificar archivo, 2- compilarla, 3-  guardar el css
 
   src('src/scss/app.scss')
-    .pipe(sass())
+    .pipe(sass({ outputStyle: 'compressed'})) // esto lo que hace es comprimir la carpeta de css, (minificar)
     .pipe(dest('build/css'))
   done()
 }
@@ -18,3 +19,5 @@ function dev () {
 
 exports.css = css;
 exports.dev = dev;
+
+/* Estas lineas de codigo son mandatorias en todos los proyectos permiten el uso de gulp con sass */
