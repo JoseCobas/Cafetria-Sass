6911af06d094f9b1dd0a66f10a2e05b7f1f44eb8
Jose Cobas <julgranen7@gmail.com>
2022-02-23 09:10:31 +0100

    added a watch to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
-function tarea (done) {
-  console.log('desde mi primer tarea....');
-  done();
+
+function css (done) {
+  // compilar sass
+  // pasos: 1- identificar archivo, 2- compilarla, 3-  guardar el css
+
+  src('src/scss/app.scss')
+    .pipe(sass())
+    .pipe(dest('build/css'))
+  done()
 }
-exports.tarea = tarea;
+
+function dev () {
+  watch('src/scss/app.scss', css);
+}
+
+exports.css = css;
+exports.dev = dev;
